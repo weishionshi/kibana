8a083755274495d2fe91f1a58e50b35f0b89062c
weishionshi <weishionshi@163.com>
2017-05-22 17:56:06 +0800

    add comment to understand code better

--- src/ui/public/routes/routes.js
+++ src/ui/public/routes/routes.js
@@ -4,7 +4,7 @@ import uiModules from 'ui/modules';
 let defaultRouteManager = new RouteManager();
 
 module.exports = {
-  ...defaultRouteManager,
+  ...defaultRouteManager,//...运算符，作用是将数组 转换成 函数入参 
   enable() {
     uiModules
     .get('kibana', ['ngRoute'])
